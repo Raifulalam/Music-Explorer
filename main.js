0f4ec00fced57a32b58bc5a065f30f139292e975
Raifulalam <raifulalam0123@gmail.com>
2024-07-12 17:56:24 +0545

    deleted index.js

--- main.js
+++ main.js
@@ -26,3 +26,16 @@
 
 
 
+// for navbar responsive (togglenav)
+
+export function toggleNav() {
+    const nav = document.getElementById("myTopnav");
+    if (nav.className === "topnav") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "topnav";
+    }
+}
+const toggle = document.querySelector('.icon')
+toggle.addEventListener('click', toggleNav)
+toggle.style.color = 'white'
